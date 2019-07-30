aac259311ae0b34d651a4ae9352aea26c9e12025
Stupid <lxt199802@qq.com>
2019-07-30 17:49:01 +0800

    token1

--- src/main/java/com/springboot/community/mapper/UserMapper.java
+++ src/main/java/com/springboot/community/mapper/UserMapper.java
@@ -3,10 +3,14 @@ package com.springboot.community.mapper;
 import com.springboot.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
 
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token = #{token} ")
+    User findByToken(String token);
 }
